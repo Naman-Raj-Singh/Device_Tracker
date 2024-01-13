7dcc7aa6cc4a71d287e2c8dc1cb68ccab700e59f
Naman Raj Singh <76091786+Naman-Raj-Singh@users.noreply.github.com>
2024-01-13 11:11:13 +0530

    Update telegram_control.py

--- telegram_control.py
+++ telegram_control.py
@@ -8,14 +8,14 @@ bot = Bot(token=TOKEN)
 def chatID(i):
     return i.message.chat.id
     
-async def send_image(image_path, caption):
+async def send_image(image_path, caption, ID):
     image_file = InputFile(open(image_path, 'rb'))
     await bot.send_photo(chat_id=ID, photo=image_file, caption=caption)
 
-async def send_message(message):
+async def send_message(message, ID):
     await bot.send_message(chat_id=ID, text=message)
 
-async def send_vid(video_path, caption):
+async def send_vid(video_path, caption, ID):
     # Send the video
     video = open(video_path, 'rb')
     await bot.send_video(chat_id=ID, video=video,caption=caption)
